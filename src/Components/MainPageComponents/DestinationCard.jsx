f53d32086bf38283eb5325ada8955a2cd0e242ba
Emile Kamana <e.kamana@alustudent.com>
2022-06-23 16:15:20 +0200

    Style main page featured destinations with static data

--- src/Components/MainPageComponents/DestinationCard.jsx
+++ src/Components/MainPageComponents/DestinationCard.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Image from 'next/image';
+import destinationPic from '../../../public/sample-pic.jpeg';
 
 const DestinationCard = () => {
-  return <div>DestinationCard</div>;
+  return <div className="relative h-[100vh] w-full">
+    <Image
+      className="rounded"
+      layout='fill'
+      objectFit='cover'
+      src={destinationPic} />
+    <div className="text-left absolute top-5 left-5">
+      <h3 className="text-2xl font-bold xl:text-4xl">Explore featured destinations</h3>
+      <p className="mt-4 leading-loose text-[1rem] lg:text-[1.2rem] xl:text-[1.5rem]">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit id ullam omnis. Minus cupiditate at sit suscipit aperiam earum libero ipsa! Quaerat natus architecto nostrum aut vero, illo fuga qui?
+      </p>
+    </div>
+  </div>;
 };
 
 export default DestinationCard;
--- src/Components/MainPageComponents/FeaturedDestinations.jsx
+++ src/Components/MainPageComponents/FeaturedDestinations.jsx
@@ -2,20 +2,25 @@ import React from "react";
 import DestinationCard from "./DestinationCard";
 
 const FeaturedDestinations = () => {
-  return <div className="bg-[#000107] font-syne text-white">
-    <div className="mt-[1rem] text-center">
-      <div className="max-w-[90vw]">
-        <h3 className="text-3xl md:text-4xl lg:text-5xl text-[#418D89] font-semibold">Explore featured destinations</h3>
+  return <div className="bg-[#000107] font-syne flex justify-center text-center text-white">
+    <div className="mt-[1rem] max-w-[90vw]">
+      <div className="">
+        <h3 className="text-3xl lg:text-4xl xl:text-6xl text-[#418D89] font-semibold">Explore featured destinations</h3>
       </div>
-      <p className="mt-4 px-4 leading-loose text-[1rem] md:text-[1rem] lg:text-[1.3rem] lg:px-[4rem]">
+      <p className="mt-2 leading-loose text-[1rem] lg:text-[1.2rem] xl:text-[2rem] ">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit id ullam omnis. Minus cupiditate at sit suscipit aperiam earum libero ipsa! Quaerat natus architecto nostrum aut vero, illo fuga qui?
       </p>
 
-      <div className="grid grid-cols-2 md:grid-cols-3">
-        <DestinationCard />
+      <div className="grid grid-cols-1 p-2 gap-6 md:grid-cols-2">
+      {destinations.map(destination => {
+        return <DestinationCard />
+      })}
       </div>
     </div>
   </div>;
 };
+
+let destinations = [1,2,3,4,5]
+
 
 export default FeaturedDestinations;
