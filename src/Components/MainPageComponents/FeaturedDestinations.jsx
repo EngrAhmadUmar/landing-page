6aeb7314db4b033956365a3de34be6acba67f0b7
demizere <l.izere@alustudent.com>
2022-06-26 21:34:42 +0200

    planning visa, areas of conservation and destination cards

--- src/Components/MainPageComponents/FeaturedDestinations.jsx
+++ src/Components/MainPageComponents/FeaturedDestinations.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import DestinationCard from "./DestinationCard";
 
 const FeaturedDestinations = () => {
-  return <div>FeaturedDestinations</div>;
+  return <div className="bg-[#000107] font-syne flex justify-center text-center text-white">
+    <div className="mt-[1rem] max-w-[90vw]">
+      <div className="">
+        <h3 className="text-3xl lg:text-4xl xl:text-6xl text-[#418D89] font-semibold">Explore featured destinations</h3>
+      </div>
+      <p className="mt-2 leading-loose text-[1rem] lg:text-[1.2rem] xl:text-[2rem] ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit id ullam omnis. Minus cupiditate at sit suscipit aperiam earum libero ipsa! Quaerat natus architecto nostrum aut vero, illo fuga qui?
+      </p>
+
+      <div className="grid grid-cols-1 p-2 gap-6 md:grid-cols-2">
+      {destinations.map(destination => {
+        return <DestinationCard />
+      })}
+      </div>
+    </div>
+  </div>;
 };
+
+let destinations = [1,2,3,4,5]
+
 
 export default FeaturedDestinations;
--- src/Components/MainPageComponents/HeroSection.jsx
+++ src/Components/MainPageComponents/HeroSection.jsx
@@ -1,7 +1,76 @@
-import React from "react";
+// import Image from "next/image";
+// import Link from "next/link";
+// import styles from "../../../styles/Home.module.css";
+// import Head from "next/head";
+
+// const HeroSection = () => {
+//   return (
+//     <div className="text-white flex justify-left">
+//       <Head>
+//         <title>Welcome to GGV</title>
+//         <meta name="description" content="Welcome to GGV" />
+//       </Head>
+//       <div className=" w-[60px] h-50 py-5">
+//         <Image src="/logo.svg" layout="responsive" width={5} height={5} />
+//       </div>
+//       <div className=" mt-[8vh]">
+//         <h3
+//           className={`${
+//             styles.header
+//           } ${"min-w-[300px] max-w-[500px] font-semibold text-[#418d89]"}`}
+//         >
+//           GGV
+//         </h3>
+//         <p className="text-2xl md:text-3xl lg:text-5xl font-semibold">
+//           Your gateway to <br></br>environmental tourism
+//         </p><br></br>
+//         <p className="md:text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus...</p>
+//         <button className="bg-[#000107] md:bg-[#418d89] rounded-sx border border-[#418d89] mt-8 mb-3 py-1">
+//           <Link href="#joinUs">
+//             <a className="p-6 text-xl md:text-2xl">Apply for the Visa</a>
+//           </Link>
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../../../styles/Home.module.css";
+import Head from "next/head";
 
 const HeroSection = () => {
-  return <div>HeroSection</div>;
+  return (
+    <div className="text-white h-[100vh]">
+      <Head>
+        <title>Welcome to GGV</title>
+        <meta name="description" content="Welcome to GGV" />
+      </Head>
+      <div className=" ml-[1.2rem] md:ml-[5rem] lg:ml-[5rem] w-[60px] h-50 py-5">
+        <Image src="/logo.svg" layout="responsive" width={5} height={5} />
+      </div>
+      <div className="ml-[1.2rem] md:ml-[5rem] lg:ml-[5rem] mt-[8vh]">
+        <h3
+          className={`${
+            styles.header
+          } ${"min-w-[300px] max-w-[500px] font-semibold"}`}
+        >
+          GGV
+        </h3>
+        <p className="text-4xl md:text-4xl lg:text-6xl font-semibold">
+          Your gateway to <br></br>environmental tourism
+        </p><br></br>
+        <p className="md:text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus...</p>
+        <button className="bg-[#000107] md:bg-[#418d89] rounded-sm border border-[#418d89] mt-12 mb-3 py-1">
+          <Link href="#Apply">
+            <a className="p-6 text-xl md:text-2xl">Apply for the Visa</a>
+          </Link>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default HeroSection;
+
