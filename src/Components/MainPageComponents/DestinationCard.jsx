c45610bf752b79757a4a3997bd95edf59b75abc0
demizere <l.izere@alustudent.com>
2022-06-27 09:35:49 +0200

    planning for visa

--- src/Components/MainPageComponents/DestinationCard.jsx
+++ src/Components/MainPageComponents/DestinationCard.jsx
@@ -1,21 +1,7 @@
 import React from "react";
-import Image from 'next/image';
-import destinationPic from '../../../public/sample-pic.jpeg';
 
 const DestinationCard = () => {
-  return <div className="relative h-[100vh] w-full">
-    <Image
-      className="rounded"
-      layout='fill'
-      objectFit='cover'
-      src={destinationPic} />
-    <div className="text-left absolute top-10 left-5">
-      <h3 className="text-2xl font-bold xl:text-4xl">Explore featured destinations</h3>
-      <p className="mt-4 leading-loose text-[1rem] lg:text-[1.2rem] xl:text-[1.5rem]">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit id ullam omnis. Minus cupiditate at sit suscipit aperiam earum libero ipsa! Quaerat natus architecto nostrum aut vero, illo fuga qui?
-      </p>
-    </div>
-  </div>;
+  return <div>DestinationCard</div>;
 };
 
 export default DestinationCard;
