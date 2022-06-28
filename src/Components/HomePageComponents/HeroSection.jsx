905a85051a66300d9a52b5d1de09a359d2b82632
Samuel wanza <s.wanza@alustudent.com>
2022-06-28 11:14:18 +0200

    updates

--- src/Components/HomePageComponents/HeroSection.jsx
+++ src/Components/HomePageComponents/HeroSection.jsx
@@ -10,25 +10,37 @@ const HeroSection = () => {
         <title>Welcome to GGV</title>
         <meta name="description" content="Welcome to GGV" />
       </Head>
-      <div className="ml-[5rem] w-[60px] h-50 py-5">
-        <Image src="/logo.svg" layout="responsive" width={5} height={5} />
-      </div>
-      <div className="ml-[5rem] mt-[8vh]">
-        <h3
-          className={`${
-            styles.header
-          } ${"min-w-[300px] max-w-[500px] font-semibold"}`}
-        >
-          Global Green Visa
-        </h3>
-        <p className="text-xl md:text-3xl lg:text-4xl">
-          Your gateway to environmental tourism
-        </p>
-        <button className="bg-[#418d89] rounded-sm mt-8 mb-3 py-1">
-          <Link href="#joinUs">
-            <a className="p-6 text-xl md:text-2xl">Join Us</a>
-          </Link>
-        </button>
+      <div className={styles.heros}>
+        <Image
+          alt="heros"
+          src="/heros.jpg"
+          layout="fill"
+          objectFit="cover"
+          quality={100}
+        />
+        <div className="lg:w-[120px] lg: ml-[2rem]  w-[60px] h-50 py-5 ">
+          <Image src="/logo.svg" layout="responsive" width={5} height={5} opacity={100}/>
+        </div>
+        <div className={styles.headerline}></div>
+          <div className="ml-[5rem] mt-[8vh]">
+            
+            <h3
+              className={`${
+                styles.headerprops
+              } ${"min-w-[300px] max-w-[500px] font-semibold"}`}
+            >
+              Global Green Visa
+            </h3>
+            <p className={styles.innerhero}>
+              Your gateway to environmental tourism
+            </p>
+            <button className={styles.innerhero__one}>
+              <Link href="#joinUs">
+                <a className={styles.innerhero__one__one}>Join Us</a>
+              </Link>
+            </button>
+          </div>
+          
       </div>
     </div>
   );
