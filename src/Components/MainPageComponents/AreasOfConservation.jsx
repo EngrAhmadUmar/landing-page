e891bd16e7e754121acfbdc1ecd0bf1f61e7b74d
Adrine UWERA <>
2022-07-05 14:46:13 +0200

    changing branches

--- src/Components/MainPageComponents/AreasOfConservation.jsx
+++ src/Components/MainPageComponents/AreasOfConservation.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "../../../styles/Home.module.css";
 
-const AreasOfConservation = () => {
+const AreasOfConservation = ({ areas }) => {
   // const { loading, error, data } = useQuery(InvestmentAreas)
   // if (loading) return <p>Loading...</p>
   // if (error) {
@@ -12,89 +12,65 @@ const AreasOfConservation = () => {
   //   return <p>Couldn't fetch</p>
   // }
 
-  // console.log(areas);
+  // console.log(data)
+  // const areas = data.conservationAreas.data
+  console.log(areas);
 
   return (
-    <div className="bg-[#faf9f6] font-syne text-black flex flex-col justify-center items-center ">
-      <div className="max-w-[90vw]">
-        <Head>
-          <title>Welcome to GGV</title>
-          <meta name="description" content="A little bit about Us" />
-        </Head>
-        <div className="pt-[5rem]">
-          <div className="max-w-[90vw]">
-            <h3
-              className="text-center text-3xl lg:text-4xl text-[#418D89] font-semibold">
-              Areas of Conservation
-            </h3>
-            {/* <div className={`${styles.header} ${"text-center w-[80vw]"}`}></div> */}
-          </div>
-
-          <p className="mt-3 md:mt-4 text-center px-2 text-xl leading-loose ml-2 md:text-2xl md:leading-relaxed ">
-            At GGV we aim to conserve the environment via sustainable tourism.
-            With every fee that you pay on the visa, it is directed to preserve
-            the following areas.
-          </p>
+    <div className="font-syne text-white">
+      <Head>
+        <title>Welcome to GGV</title>
+        <meta name="description" content="A little bit about Us" />
+      </Head>
+      <div className="mt-[7rem]">
+        <div className="max-w-[90vw]">
+          <h3
+            className={`${
+              styles.headings
+            }  ${"text-center text-4xl font-semibold"}`}
+          >
+            Areas of Conservation
+          </h3>
+          {/* <div className={`${styles.header} ${"text-center w-[80vw]"}`}></div> */}
         </div>
 
-        <div className="w-full flex flex-col justify-content items-center">
-          <div className="grid grid-cols-1 p-2 gap-12 mt-[4rem] md:grid-cols-2 lg:grid-cols-3 ">
-            {areas.map((area) => (
-              <div
-                key={area.id}
-                className="w-full h-[35rem] md:w-[23rem] lg:w-[25rem]  p-2 rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl"
-              >
-                <div className="p-2">
-                  <Image src={area.image} alt="" width={500} height={300}></Image>
-                  <h2 className="my-4 text-2xl font-bold xl:text-4xl max-w-[18rem]">
-                    {area.title}
-                  </h2>
-                  <p className=" text-lg lg:text-xl leading-relaxed">
-                    {area.short_description.substring(0, 200)}
-                  </p>
-                </div>
+        <p className="text-center mt-8 px-10 leading-loose  tracking-[.01em] text-[1.3rem] md:text-[1.5rem] lg:text-[2rem] lg:px-[4rem]">
+          At GGV we aim to conserve the environment via sustainable tourism.
+          With every fee that you pay on the visa, it is directed to preserve
+          the following areas.
+        </p>
+      </div>
+
+      <div className="w-full flex flex-col justify-content items-center">
+        <div className="my-10 w-11/12 gap-12 flex-wrap flex justify-center items-center">
+          {areas.map((area) => (
+            <div
+              key={area.id}
+              className="w-96 md:w-[23rem] lg:w-[25rem]  p-2 rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl"
+            >
+              <div className="p-2">
+                <Image src="/an1.svg" alt="" width={500} height={300}></Image>
+                <h2 className="font-bold my-4 text-[1.3rem] md:text-[1.5rem] lg:text-[2rem]">
+                  {area["attributes"].title}
+                </h2>
+                <p className=" text-[1rem] md:text-[1.2rem] lg:text-[1.5rem]">
+                  {area["attributes"].short_description.substring(0, 200)}
+                </p>
               </div>
-            ))}
-          </div>
-
-          <button className="bg-[#418d89] py-2 rounded-lg mt-8 mb-3 py-1">
-            <Link href="#joinUs">
-              <a className="px-20 text-lg md:text-xl lg:text-2xl">
-                Apply for the Visa
-              </a>
-            </Link>
-          </button>
+            </div>
+          ))}
         </div>
+
+        <button className="bg-[#418d89] py-2 rounded-lg mt-8 mb-3 py-1">
+          <Link href="#joinUs">
+            <a className="px-20 text-lg md:text-xl lg:text-2xl">
+              Apply for the Visa
+            </a>
+          </Link>
+        </button>
       </div>
     </div>
   );
 };
-
-let areas = [
-  {
-    title: "MegaFauna",
-    image: "/an1.svg",
-    short_description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit id ullam omnis. Minus cupiditate at sit suscipit aperiam earum libero ipsa! Quaerat natus architecto nostrum aut vero, illo fuga qui?"
-  },
-  {
-    title: "Water protection",
-    image: "/an2.svg",
-    short_description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit id ullam omnis. Minus cupiditate at sit suscipit aperiam earum libero ipsa! Quaerat natus architecto nostrum aut vero, illo fuga qui?"
-  },
-  {
-    title: "Bird Conservation",
-    image: "/an3.svg",
-    short_description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit id ullam omnis. Minus cupiditate at sit suscipit aperiam earum libero ipsa! Quaerat natus architecto nostrum aut vero, illo fuga qui?"
-  },
-  {
-    title: "Forest Restoration",
-    image: "/an4.svg",
-    short_description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit id ullam omnis. Minus cupiditate at sit suscipit aperiam earum libero ipsa! Quaerat natus architecto nostrum aut vero, illo fuga qui?"
-  }
-];
 
 export default AreasOfConservation;
