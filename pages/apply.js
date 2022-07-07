4d4c237555aed8bda2b1da02dd79ba0765c4dc3b
José Orlando Cumbe <j.cumbe@alustudent.com>
2022-07-07 18:17:01 +0200

    form data types fix

--- pages/apply.js
+++ pages/apply.js
@@ -1,13 +1,8 @@
+
 import React from "react";
-<<<<<<< HEAD
 // import { Link } from "react-router-dom";
 import { ApplyForm } from "../src/Components/ApplyPageComponents/Index";
 
-=======
-import { ApplyForm } from "../src/Components/ApplyPageComponents/Index";
-import Login from "../src/Components/ApplyPageComponents/LoginForm";
-import { AUTH_TOKEN } from "../src/Components/constant";
->>>>>>> ef66d41721fd4029aab6f86ec54ecec9f80c8717
 export default function Apply() {
   const [token, setToken] = React.useState(null);
   React.useEffect(() => {
