e1707d65c6d3b8bc0487ceb1e7db64478458fcbd
JustusWamswa <93210719+JustusWamswa@users.noreply.github.com>
2022-07-08 12:26:57 +0400

    added short desc

--- src/Components/MainPageComponents/Map.jsx
+++ src/Components/MainPageComponents/Map.jsx
@@ -9,24 +9,31 @@ const markers = [
     name: "Gorilla Lake in Gisenyi",
     position: {lat: -1.70278, lng: 29.25639},
     image: "/sample-pic.jpeg",
-  },
+    short_description: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+  }, 
   {
     id: 2,
     name: "Akagera National Park",
     position: {lat: -1.6333308, lng: 30.7833302},
     image: "/akagera.jpg",
+    short_description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. "
+
   },
   {
     id: 3,
     name: "Fazenda Sengha",
     position: {lat: -1.6154808935029508, lng: 29.945828955849198},
     image: "/fazhenda.jpg",
+    short_description: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+
   },
   {
     id: 4,
     name: "Gorilla Nest",
     position: {lat: -1.3673198545680096, lng: 29.575345442511793},
     image: "/gorilla.jpg",
+    short_description: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+
   }
 ];
 
@@ -53,205 +60,77 @@ function Map() {
     <GoogleMap
       onLoad={handleOnLoad}
       onClick={() => setActiveMarker(null)}
-      mapContainerStyle={{ width: "100vw", height: "100vh" }}
+      mapContainerStyle={{ width: "100%", height: "100vh" }}
     >
-      {markers.map(({ id, name, position, image }) => (
+      
+      {markers.map(({ id, name, position, image, short_description }) => (
+          
         <Marker
           key={id}
           position={position}
           onClick={() => handleActiveMarker(id)}
         >
+          
           {activeMarker === id ? (
-            <InfoWindow onCloseClick={() => setActiveMarker(null)}>
+            <InfoWindow
+            onCloseClick={() => setActiveMarker(null)}>
               <Link href={name}>
-              <div className="flex flex-col cursor-pointer font-semibold text-[#418D89] hover:text-blue-600">
-                {name} 
+              <div className="flex flex-col w-32 cursor-pointer font-semibold text-center text-[#418D89] hover:text-blue-600">
+                {name && <div className="w-32 pb-2">{name}</div>} 
                 {image && 
                   <Image
-                    className="rounded opacity-100 pt-9"
+                    className="rounded"
                     width={"100%"}
                     height={"100%"}
                     objectFit="cover"
                     src={image}
                   />
                 }
+                {short_description &&
+                  <div className="text-black text-left font-normal w-32 pt-2">
+                    {short_description}
+                  </div>
+                }
               </div>
               </Link>
             </InfoWindow>
-          ) : null}
+          ): null}
         </Marker>
+        
       ))}
     </GoogleMap>
-  ): <></>;
+  ): null;
 }
 
 export default Map;
 
-//  import React from "react";
-//  import {
-//    GoogleMap,
-//    useLoadScript,
-//    Marker,
-//    InfoWindow,
-//  } from "@react-google-maps/api";
-//  import { formatRelative } from "date-fns";
 
+// export async function getStaticProps() {
 
-//  const libraries = ["places"];
-//  const mapContainerStyle = {
-//    height: "100vh",
-//    width: "100vw",
-//  };
+//   const client = new ApolloClient({
+//     uri: 'http://localhost:1337/graphql',
+//     cache: new InMemoryCache()
+//   });
 
-//  const center = {
-//    lat: 43.6532,
-//    lng: -79.3832,
-//  };
-
-//  export default function Map() {
-//    const { isLoaded, loadError } = useLoadScript({
-//      googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-//      libraries,
-//    });
-//    const [markers, setMarkers] = React.useState([]);
-//    const [selected, setSelected] = React.useState(null);
-
-
-//    const mapRef = React.useRef();
-//    const onMapLoad = React.useCallback((map) => {
-//      mapRef.current = map;
-//    }, []);
-
-  
-
-//    if (loadError) return "Error";
-//    if (!isLoaded) return "Loading...";
-
-//    return (
-//      <div>
-//        <GoogleMap
-//          id="map"
-//          mapContainerStyle={mapContainerStyle}
-//          zoom={3}
-//          center={center}
-//          onLoad={onMapLoad}
-//        >
-        
-//            <Marker
-//              position={center}
-//              onMouseOver={() => {
-//                setSelected();
-//              }}
-//              icon={{
-//                origin: new window.google.maps.Point(0, 0),
-//                anchor: new window.google.maps.Point(15, 15),
-//                scaledSize: new window.google.maps.Size(30, 30),
-//              }}
-//            />
-//            <Marker
-//              position={{
-//                lat: 43.6532,
-//                lng: -79.3832,
-//              }}
-//              onMouseOver={() => {
-//                setSelected();
-//              }}
-//              icon={{
-//                origin: new window.google.maps.Point(0, 0),
-//                anchor: new window.google.maps.Point(15, 15),
-//                scaledSize: new window.google.maps.Size(30, 30),
-//              }}
-//            />
-       
-
-//          {selected ? (
-//            <InfoWindow
-//              position={center}
-//              onCloseClick={() => {
-//                setSelected(null);
-//              }}
-//            >
-//              <div>
-//                <h2>
-//                  Alert
-//                </h2>
-            
-//              </div>
-//            </InfoWindow>
-//          ) : null}
-//        </GoogleMap>
-//      </div>
-//    );
-//  }
-//  import React from 'react'
-//  import { useState } from 'react';
-//  import { GoogleMap, useJsApiLoader, Marker, InfoWindow } from '@react-google-maps/api';
-
-//  const containerStyle = {
-//      width: '100%',
-//      height: '100vh'
-//  };
-
-//  const center = {
-//      lat: -1.9441,
-//      lng: 30.0619
-//  };
-
-//  function Map() {
-//      const { isLoaded } = useJsApiLoader({
-//          googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY
-//      })
-
-//      const [map, setMap] = useState(null)
-//      const [selected, setSelected] = useState(null);
-
-//      const onLoad = React.useCallback(function callback(map) {
-//          const bounds = new window.google.maps.LatLngBounds(center);
-//          map.fitBounds(bounds);
-//          setMap(map)
-//      }, [])
-
-//      const onUnmount = React.useCallback(function callback(map) {
-//          setMap(null)
-//      }, [])
-
-//      const divStyle = {
-//          background: `white`,
-//          border: `1px solid #ccc`,
-//          padding: 15
-//        }
-
-//      return isLoaded ? (
-//          <GoogleMap
-//              mapContainerStyle={containerStyle}
-//              center={center}
-//              zoom={10}
-//              onLoad={onLoad}
-//              onUnmount={onUnmount}
-//          >
-//              {<Marker
-//                  position={center}
-//                  onClick={() => {
-//                      setSelected();
-//                  }}
-               
-
-//              />/* Child components, such as markers, info windows, etc. */}
-//              <></>
-//              {selected ? (<InfoWindow
-                
-//                  position={center}
-//              >
-//                  <div style={divStyle}>
-//                      <h1>InfoWindow</h1>
-//                  </div>
-//              </InfoWindow>) : null}
-            
-//          </GoogleMap>
-//      ) : <></>
-//  }
-
-//  export default React.memo(Map)
-
-
-
+//   const { data } = await client.query({
+//     query: gql`
+//     query getFeaturedDestinations{
+//       future-destinations {        
+//         attributes {
+//           id
+//           title
+//           short_description
+//           images
+//           longitude
+//           latitude
+//         }
+//       }
+//     }
+//     `
+//   });
+//   return {
+//     props: {
+//       futuredestinations: data.future-destinations
+//     }
+//   }
+// }
