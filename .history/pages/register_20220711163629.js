72c83d6a1be99ebf73b4b9b4aa41d7dd4bd61042
BrightonMboya <b.mboya@alustudent.com>
2022-07-11 16:36:40 +0200

    created a new branch

---
+++ .history/pages/register_20220711163629.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { RegisterForm } from "../src/Components/ApplyPageComponents/Index";
+
+export default function Login() {
+  return (
+    <React.Fragment>
+      <div className="bg-[#d1be84] h-[100vh]">
+        <RegisterForm />
+      </div>
+    </React.Fragment>
+  );
+}
